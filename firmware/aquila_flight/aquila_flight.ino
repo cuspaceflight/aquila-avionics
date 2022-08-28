a557d81192286bf6724e0fd20f284df1322e03a2
Daniel Fearn <dlhfearn@gmail.com>
2022-08-28 20:34:25 +0100

    made the testing output more interesting

--- firmware/aquila_flight/aquila_flight.ino
+++ firmware/aquila_flight/aquila_flight.ino
@@ -7,9 +7,12 @@
  * 
  */
 
+#define SEA_LEVEL 1025
+
 #include "aquila_interface.h"
 
 AQUILA aquila;
+
 
 void setup() {
   Serial.begin(19200);
@@ -17,40 +20,38 @@ void setup() {
 }
 
 void loop() {
+  aquila.update_accel();
+  aquila.poll_baro_ext();
+  aquila.poll_baro_int();
+
   char timestamp[32];
   aquila.rtc_datetime(timestamp);
   Serial.println(timestamp);
-  Serial.println(aquila.rtc_unix());
-  delay(500);
-  /*
-  aquila.update_accel();
+
+  Serial.print("Accelerations: ");
   Serial.print(aquila.get_accel_x(), 4);
   Serial.print(", ");
   Serial.print(aquila.get_accel_y(), 4);
   Serial.print(", ");
   Serial.println(aquila.get_accel_z(), 4);
-  delay(10);
-  */
-  
-  /*
-  if(aquila.poll_baro_ext()){
-    Serial.print(aquila.get_ext_temperature());
-    Serial.print(", ");
-    Serial.print(aquila.get_ext_pressure());
-    Serial.print(", ");
 
-    float abs_altitude = 44330*(1-pow((aquila.get_ext_pressure()/1013.25), (1/5.255)));
-    Serial.println(abs_altitude);
-  }*/
+  Serial.print("External barometer: ");
+  Serial.print(aquila.get_ext_temperature());
+  Serial.print(", ");
+  Serial.print(aquila.get_ext_pressure());
+  Serial.print(", ");
+  Serial.println(abs_altitude(aquila.get_ext_pressure()));
   
-  /*
-  if(aquila.poll_baro_int()){
-    Serial.print(aquila.get_int_temperature());
-    Serial.print(", ");
-    Serial.print(aquila.get_int_pressure());
-    Serial.print(", ");
+  Serial.print("Internal barometer: ");
+  Serial.print(aquila.get_int_temperature());
+  Serial.print(", ");
+  Serial.print(aquila.get_int_pressure());
+  Serial.print(", ");
+  Serial.println(abs_altitude(aquila.get_int_pressure()));
+  Serial.println();
 
-    float abs_altitude = 44330*(1-pow((aquila.get_int_pressure()/1013.25), (1/5.255)));
-    Serial.println(abs_altitude);
-  }*/
+}
+
+float abs_altitude(float pressure) {
+  return 44330*(1-pow((pressure/SEA_LEVEL), (1/5.255)));
 }
