ac13b7650208b3adbe693a2420c63cf58522a74c
Daniel Fearn <dlhfearn@gmail.com>
2022-10-09 12:17:13 +0100

    added timing loops

--- firmware/aquila_flight/aquila_flight.ino
+++ firmware/aquila_flight/aquila_flight.ino
@@ -11,15 +11,62 @@
 
 AQUILA aquila;
 
+enum FlightState {LOCKED, PAD, BURN, COAST, APOGEE, DESCENT, LAND};
+FlightState state;
+
+// timers for 10Hz and 100Hz loops
+uint32_t hz10_time;
+uint32_t hz100_time;
+uint32_t curr_time;
 
 void setup() {
+  state = LOCKED;
 
+  Serial.begin(19200);
+
+  hz10_time = hz100_time = micros();
 }
 
 void loop() {
-
+  curr_time = micros();
+  if (curr_time - hz10_time > 1e5) {
+    hz10_time = curr_time;
+    hz10();
+  }
+  if (curr_time - hz100_time > 1e4) {
+    hz100_time = curr_time;
+    hz100();
+  }
 }
 
-float abs_altitude(float pressure, float sea_level) {
-  return 44330*(1-pow((pressure/sea_level), (1/5.255)));
+void hz10(){
+  Serial.println("10");
+}
+
+void hz100(){
+  Serial.println("100");
+  // state switching
+  switch(state) {
+    case LOCKED:
+
+      break;
+    case PAD:
+
+      break;
+    case BURN:
+
+      break;
+    case COAST:
+
+      break;
+    case APOGEE:
+
+     break;
+    case DESCENT:
+
+      break;
+    case LAND:
+
+      break;
+  }
 }
