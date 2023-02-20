66b62cbdcbd92fa931acb9eca052cb65dc07aef2
Daniel Ciesla <dac90@cam.ac.uk>
2023-02-20 23:04:24 +0000

    Added comments and corrections

--- firmware/aquila_interface/rfm95w.cpp
+++ firmware/aquila_interface/rfm95w.cpp
@@ -1,9 +1,14 @@
 /* Daniel Ciesla
+20/02/2023
 LoRa rfm95w chip
+
+This Class interacts with the rfm95w LoRa chip, transmitting and receiving an array of uint8_t.
+Inputs and Outputs are pointers to such arrays.
 */
 
 #include <rfm95w.h>
 
+//Attempt activation of LoRa, return true if success, false is fail
 bool lora_setup(){
     if (!LoRa.begin(lora_frequency)) {
         Serial.println("LoRa failed");
@@ -19,16 +24,17 @@ bool lora_setup(){
     
 }
 
+//Receive data, output as pointer to array of uint8_ts
 int* RFM95W::lora_receive(){
     int current_packet_size = LoRa.parsePacket();
     Serial.println(current_packet_size);
     if (current_packet_size > 0) { 
-        if (current_packet_size != lora_packet_size){
+        if (current_packet_size != packet_size){
             Serial.print(" Packet Wrong Size ");
         }
-        static uint8_t lora_array[lora_packet_size];
+        static uint8_t lora_array[packet_size];
         uint8_t current_byte;
-        for (i=0, i<lora_packet_size, i++){
+        for (i=0, i<packet_size, i++){
             if (LoRa.available()) {
                 current_byte = LoRa.read();
                 lora_array[i] = current_byte;
@@ -42,22 +48,25 @@ int* RFM95W::lora_receive(){
     }
 }
 
+//Transmit data in array pointed to by input
 void RFM95W::lora_transmit(uint8_t* lora_array){
     LoRa.beginPacket();
-    for (i=0, i<lora_packet_size, i++){
+    for (i=0, i<packet_size, i++){
         LoRa.write(lora_array[i]);
     }
     LoRa.endPacket();
     Lora.receive();
 }
 
-int* RFM95W::lora_update(uint8_t* lora_transmit_pointer){
-    uint8_t* lora_receive_pointer;
-    lora_receive_pointer = lora_receive();
-    lora_transmit(lora_transmit_pointer);
-    return lora_receive_pointer;
+//Receive and Transmit
+int* RFM95W::lora_update(uint8_t* data_out){
+    uint8_t* data_in;
+    data_in = lora_receive();
+    lora_transmit(data_out);
+    return data_in;
 }
 
+//Print various radio signal quality indicators (might convert to output later)
 void lora_quality(){
     Serial.println("RSSI: ");
     Serial.print(LoRa.packetRssi());     
--- firmware/aquila_interface/rfm95w.h
+++ firmware/aquila_interface/rfm95w.h
@@ -7,11 +7,11 @@ LoRa rfm95w  chip
 #include <LoRa.h>
 
 #define lora_frequency 868E6
-#define lora_packet_size 16
+#define packet_size 16
 
 class RFM95W{
     public:
-    void lora_setup();
+    bool lora_setup();
     int* lora_receive();
     void lora_transmit();
     int* lora_update();
